85fdf549d0b3714dbc5ebc6bcade690d095abb6e
MagicPrince666 <846863428@qq.com>
2024-08-10 13:49:42 +0000

    update

--- launch/rviz_MID360_launch.py
+++ launch/rviz_MID360_launch.py
@@ -13,11 +13,8 @@ output_type   = 0
 frame_id      = 'livox_frame'
 lvx_file_path = '/home/livox/livox_test.lvx'
 cmdline_bd_code = 'livox0000000001'
-
-cur_path = os.path.split(os.path.realpath(__file__))[0] + '/'
-cur_config_path = cur_path + '../config'
-rviz_config_path = os.path.join(cur_config_path, 'display_point_cloud_ROS2.rviz')
-user_config_path = os.path.join(cur_config_path, 'MID360_config.json')
+rviz_config_path = os.path.join(get_package_share_directory('chassis'), 'config', 'display_point_cloud_ROS2.rviz')
+user_config_path = os.path.join(get_package_share_directory('chassis'), 'config', 'MID360_config.json')
 ################### user configure parameters for ros2 end #####################
 
 livox_ros2_params = [
